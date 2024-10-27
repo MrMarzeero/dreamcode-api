a50781f3b1c3384473edd95a567135a8b8b39fc7
MrMarzeero <otaviorp2014@gmail.com>
2024-10-27 18:37:28 -0300

    CORS working in .env ALLOWED_ORIGIN variable

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import router from './routes';
 import { config } from 'dotenv';
 import swaggerUi from 'swagger-ui-express';
 import swaggerDocs from './swagger.json';
+import cors from 'cors';
 
 config();
 
@@ -12,6 +13,7 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use('/', router);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+app.use(cors());
 
 app.listen(port, () => {
   console.log(`Server working on ${port}!`);
