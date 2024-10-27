d0df36bd91160d7836b2a54375abcac2d1a2dac9
MrMarzeero <otaviorp2014@gmail.com>
2024-10-27 14:45:17 -0300

    imported and setup swagger

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import router from './routes';
 import { config } from 'dotenv';
 import swaggerUi from 'swagger-ui-express';
+import swaggerDocs from './swagger.json'
 
 config();
 
@@ -9,8 +10,8 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use('/generate', router);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup())
+app.use('/', router);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 
 app.listen(port, () => {
   console.log(`Server working on ${port}!`);
