14f4a2f6f0bdd71a6ab27960c59f266abcf7845c
MrMarzeero <otaviorp2014@gmail.com>
2024-10-29 17:08:33 -0300

    cors open

--- src/index.ts
+++ src/index.ts
@@ -17,9 +17,7 @@ const limiter = rateLimit({
 })
 
 app.use(limiter)
-app.use(cors({
-  origin: process.env.ALLOWED_ORIGIN
-}));
+app.use(cors())
 app.use(express.json());
 app.use('/', router);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
