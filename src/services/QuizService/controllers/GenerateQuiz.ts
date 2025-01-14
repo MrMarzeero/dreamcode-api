e115a6de635142bfa2c3d19a73b92a202740e003
MrMarzeero <otaviorp2014@gmail.com>
2025-01-14 11:17:37 -0300

    adding things to QService

---
+++ src/services/QuizService/controllers/GenerateQuiz.ts
@@ -0,0 +1,33 @@
+import openai from '../../../lib/openai';
+import { Request, Response } from 'express';
+import { generateQuizPrompt } from '../prompts';
+import { genProblemSchema, genSolutionSchema, genTestCasesSchema } from '../../../validation';
+import { badRequest, internalServerError, generationSuccess } from '../../../helpers';
+
+export class QuizController {
+    static async generateQuiz(req: Request, res: Response) {
+        try {
+            genQuizSchema.parse(req.body);
+          } catch (err) {
+            if (err instanceof Error) return res.status(400).json(badRequest(err));
+          }
+          try {
+            const completion = await openai.chat.completions.create({
+              model: 'gpt-4o-mini',
+              messages: [
+                { role: 'system', content: generateQuizPrompt },
+                { role: 'user', content: JSON.stringify(req.body) },
+              ],
+            });
+      
+            const messageContent = completion.choices[0].message.content;
+      
+            if (messageContent != null) {
+              const jsonResponse = JSON.parse(messageContent);
+              return res.status(201).json(generationSuccess(jsonResponse));
+            }
+          } catch (err) {
+            if (err instanceof Error) return res.status(500).json(internalServerError(err));
+          }
+    }
+}
--- src/services/QuizService/controllers/GenerateQuizz.ts
+++
@@ -1,8 +0,0 @@
-import openai from '../../../lib/openai'
-import { Request, Response } from 'express';
-
-export class QuizController {
-    static async generateQuiz(req: Request, res: Response) {
-
-    }
-}
