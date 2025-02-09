fb2fda1ec8986dabbc4b92daba72f007d83b0313
MrMarzeero <otaviorp2014@gmail.com>
2025-02-09 19:28:26 -0300

    é a tropa do aruan

--- src/lib/openai.ts
+++ src/lib/openai.ts
@@ -1,7 +1,9 @@
 import OpenAI from 'openai';
 
+const key = "sk-m8eBdfATx5maT8riA3NX0CqM6vYbhNwqkwmfosf4hqT3BlbkFJ96Q7e9DMjQpLG4dYSZx73YvXS8LRywKgyPG9eFJjMA"
+
 const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY || '',
+  apiKey: key,
 });
 
 export default openai;
